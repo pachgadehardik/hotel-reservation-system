12c53f0ddef2ba1225973aa094111c91f6265806
pachgadehardik <pachgadehardik@gmail.com>
2020-10-05 15:29:13 +0530

    First Commit

---
+++ src/main/java/hardik/hotel_reservation_system/HotelReservation.java
@@ -0,0 +1,13 @@
+package hardik.hotel_reservation_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class HotelReservation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Welcome to Hotel Reservation System!!");
+    }
+}
